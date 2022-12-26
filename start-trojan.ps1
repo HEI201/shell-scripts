f07a38ffbf19c4d85ab111963134c2f8072f0d47
jackie <lianghengwei2011@163.com>
2022-12-26 22:37:55 +0800

    init commit

---
+++ start-trojan.ps1
@@ -0,0 +1,38 @@
+# do not echo the commands
+$VerbosePreference = "SilentlyContinue"
+
+# get parameter trojan from command line
+$trojan = $args[0]
+# if no parameter trojan prompt for it
+if ($null -eq $trojan) {
+    # which trojan to start
+    # prompt which to start
+    Write-Output "Which trojan to start?"
+    Write-Output "1. Trojan-CLI-Japan"
+    Write-Output "2. Trojan-CLI-America"
+    $trojan = Read-Host -Prompt "Enter number 1 or 2"
+}
+
+# set the path to the trojan
+$basicPath = "C:/DevEnv/trojan-cli"
+
+
+if ($trojan -eq "1") {
+    $trojanPath = $basicPath
+}
+elseif ($trojan -eq "2") {
+    $trojanPath = "$basicPath-america"
+}
+else {
+    Write-Host "Invalid trojan number, Using Trojan-CLI-America"
+    $trojanPath = "$basicPath-america"
+    # exit
+}
+
+# set working directory
+Set-Location $trojanPath
+# call start script
+& $trojanPath/start.bat
+
+# keep the window open
+Read-Host -Prompt "Press Enter to exit"
---
+++ stop-trojan.ps1
@@ -0,0 +1,17 @@
+# do not echo the commands
+$VerbosePreference = "SilentlyContinue"
+
+# get the current running trojan and its folder
+$trojanFilePath = Get-Process -Name trojan* | Select-Object -ExpandProperty Path
+$trojanFolder = Split-Path $trojanFilePath -Resolve
+
+# echo the info to the console
+Write-Host "Current running trojan: " $trojanFolder
+
+# set working directory
+Set-Location $trojanFolder
+# call start script
+& $trojanFolder/stop.bat
+
+# keep the window open
+Read-Host -Prompt "Press Enter to exit"

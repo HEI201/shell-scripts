73015b544bf86adb53664d16e7a45041608e0753
jackie <lianghengwei2011@163.com>
2023-11-07 00:44:03 +0800

    下载到当前目录优化

--- powershell/Download-Youtube.ps1
+++ powershell/Download-Youtube.ps1
@@ -15,43 +15,46 @@ $currentPath = Get-Location
 $youtubedlPath = "D:\Dev\repo\youtube-dl"
 $env:PYTHONPATH += ";$youtubedlPath"
 
-$savedOption = ""
+$outputOption = ""
+$urlsPath = "D:\Video\youtube\youtube-urls.txt"
 # if the current switch is set, save the videos in the current directory
 if ($current) {
     # fix me: the path is not working, youtube-dl is replacing the : with #, this makes the path invalid
-    $savedOption = "-o $currentPath/%(uploader)s/%(title)s/video.%(ext)s"
+    $outputOption = "%(uploader)s/%(title)s/video.%(ext)s"
+    $urlsPath = "$currentPath/youtube-urls.txt"
 }
-
-Write-Host $currentPath
-Write-Host $savedOption
 
 function DownloadVideo {
     param (
         $url,
         $option
     )
-    python -m youtube_dl $url $option
+    if ($option) {
+        python -m youtube_dl $url -o $option
+        return
+    }
+    python -m youtube_dl $url
 }
 
 if ($url) {
     # download the video by calling python -m youtube_dl $url
-    # python -m youtube_dl $url $savedOption
-    DownloadVideo $url $savedOption
+    # python -m youtube_dl $url $outputOption
+    DownloadVideo $url $outputOption
     exit
 }
 
 
 
 # set the path to the file containing the list of URLs
-$urls = Get-Content -Path "D:\Video\youtube\youtube-urls.txt"
+$urls = Get-Content -Path $urlsPath
 
 # loop through the list of URLs
 foreach ($url in $urls) {
     # download the video by calling python -m youtube_dl $url
     # catch errors and continue with the next URL
     try {
-        # python -m youtube_dl $url $savedOption
-        DownloadVideo $url $savedOption
+        # python -m youtube_dl $url $outputOption
+        DownloadVideo $url $outputOption
     }
     catch {
         continue

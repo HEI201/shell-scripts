0d43e6cc150840b923c319bd038be8baab7f95b0
jackie <lianghengwei2011@163.com>
2023-11-08 23:57:26 +0800

    解决每个url扫码问题

--- powershell/Download-TikTok.ps1
+++ powershell/Download-TikTok.ps1
@@ -8,55 +8,80 @@ $downloaderPath = "D:\Video\tiktok\downloader"
 # add downloader path to PATH
 $env:PATH += ";$downloaderPath"
 
-# Map the network drive
-# New-PSDrive -Name $driverName -PSProvider FileSystem -Root "\\192.168.88.190\26956693" -Persist
-
-# Create a symbolic link
-# New-Item -ItemType SymbolicLink -Path "$downloaderPath\Download" -Target "$driverName`:\tiktok"
-
 # working directory is running directory
 # get working directory
 $runningDownloaderPath = (Get-Location).Path
 
 # read the config file
 $configFilePath = "$runningDownloaderPath\conf.ini"
-$ini = Get-IniContent -FilePath $configFilePath
-Write-Host $ini
 
-# read cookie from cookie.txt
-$cookie = Get-Content -Path "$downloaderPath\cookie"
-# $ini["cookie"] = $cookie
+$urlsFilepath = "$runningDownloaderPath\urls.txt"
+
+# if urls file does not exist, exit with error
+if (-not (Test-Path -Path $urlsFilepath)) {
+    Write-Host "urls file does not exist: $urlsFilepath"
+    exit 1
+}
 
 # read links from urls.txt
-$urls = Get-Content -Path "$downloaderPath\urls.txt"
+$urls = Get-Content -Path $urlsFilepath
+
+function  Get-Cookie {
+    # read cookie from ini file
+    $iniFile = Get-IniContent -FilePath $configFilePath
+    $cookie = $iniFile['_']['cookie']
+
+    # if cookie is empty, assign an empty string
+    if ($null -eq $cookie) {
+        $cookie = ""
+    }
+
+    return $cookie
+}
+
+# filter the lines that are empty and comments from urls.txt
+# read links from urls.txt and filter out empty and comment lines
+$urls = Get-Content -Path $urlsFilepath | Where-Object { $_ -notmatch "^(\s*#|$)" }
+# print links line by line
+Write-Host "links:"
+foreach ($url in $urls) {
+    Write-Host $url
+}
 
 # loop through the list of URLs
 foreach ($url in $urls) {
-    # if $url is empty, continue with the next URL
-    if ($url -eq "") {
-        continue
-    }
-
-    # if $url is a comment, continue with the next URL
-    if ($url.StartsWith("#")) {
-        continue
-    }
 
     # download the video by calling python -m TikTokDownload.py
     # catch errors and continue with the next URL
-    $ini["uid"] = $url
-    Write-Host $ini["uid"]
-
-    # remove the old config file and write the new one
+    $ini = @{
+        uid             = $url
+        music           = "no"
+        cover           = "no"
+        desc            = "no"
+        folderize       = "no"
+        update          = "no"
+        mode            = "post"
+        cookie          = Get-Cookie
+        interval        = "all"
+        # path 下载地址是相对于当前运行脚本的目录
+        path            = "./"
+        naming          = "{create}_{desc}"
+        max_connections = 5
+        max_tasks       = 2
+    }
+    
     Remove-Item -Path $configFilePath
-    Out-IniFile -InputObject $ini -FilePath  $configFilePath
+    Out-IniFile -InputObject $ini -FilePath $configFilePath
     
     try {
         TikTokTool
     }
     catch {
-        Start-Sleep -Seconds 20
+        # log the error
+        Write-Host "error downloading $url : $($_.Exception.Message)"
+        Write-Host "sleeping for 5 s ..."
+        Start-Sleep -Seconds 5
         continue
     }
 }
-exit
+exit 0
